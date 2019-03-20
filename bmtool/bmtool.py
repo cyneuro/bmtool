4ff7b49dd4b87c63710458a4fbe07c5249925d2b
Tyler Banks <tbg28@mizzou.edu>
2019-03-20 12:35:32 -0500

    parsing

--- bmtool/bmtool.py
+++ bmtool/bmtool.py
@@ -12,12 +12,15 @@ python -m bmtool.debug
 """
 
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(description=use_description, formatter_class=RawTextHelpFormatter,usage=SUPPRESS)
     options = None
     try:
+        if not len(sys.argv) > 1:
+            raise
+        if sys.argv[1] in ['-h','--h','-help','--help','help']:
+            raise
         options = parser.parse_args()
     except:
-        args, leftovers = parser.parse_known_args()
-        if not args.h:
-            parser.print_help()
+        parser.print_help()
         sys.exit(0)
