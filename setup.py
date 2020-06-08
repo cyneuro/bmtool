fedfaf1e0222e85f9d4528f3d1b61cf28631e640
Tyler Banks <tbanks@mail.missouri.edu>
2020-06-08 17:05:52 -0500

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bmtool",
-    version="0.1.8",
+    version="0.1.9",
     author="Tyler Banks",
     author_email="tbanks@mail.missouri.edu",
     description="BMTools",
