9cb4cf1dfa9c8f820897eade8266706c608d394c
Tyler Banks <tjb1991@gmail.com>
2023-03-19 00:05:35 -0500

    fix passive calculations, fi plot

--- bmtool/singlecell.py
+++ bmtool/singlecell.py
@@ -9,10 +9,9 @@ from neuron import h
 import numpy as np
 
 class CurrentClamp():
-    def __init__(self, template_name, post_init_function=None, record_sec="soma", record_loc="0.5", tstart=50, tstop=1000,
+    def __init__(self, template_name, post_init_function=None, record_sec="soma", record_loc="0.5", tstop=1000,
                  inj_sec="soma", inj_loc="0.5", inj_amp=100, inj_delay=100, inj_dur=1000):
         self.template_name = template_name
-        self.tstart = tstart
         self.tstop = tstop
         self.inj_dur = inj_dur
         
@@ -76,11 +75,11 @@ class CurrentClamp():
         return (list(self.t_vec), list(self.cell_vec))
 
 class Passive(CurrentClamp):
-    def __init__(self, template_name, post_init_function=None, record_sec="soma", record_loc="0.5", tstart=50, tstop=1000,
-                 inj_sec="soma", inj_loc="0.5", inj_amp=-100,inj_delay=200, inj_dur=1000,):
+    def __init__(self, template_name, post_init_function=None, record_sec="soma", record_loc="0.5", tstop=1200,
+                 inj_sec="soma", inj_loc="0.5", inj_amp=-100,inj_delay=200, inj_dur=1000):
         super(Passive, self).__init__(template_name=template_name,post_init_function=post_init_function,
                                       record_sec=record_sec, record_loc=record_loc,
-                                      tstart=tstart, tstop=tstop, inj_sec=inj_sec, inj_loc=inj_loc,
+                                      tstop=tstop, inj_sec=inj_sec, inj_loc=inj_loc,
                                       inj_amp=inj_amp, inj_delay=inj_delay, inj_dur=inj_dur)
 
         self.cell_v_final = 0
@@ -100,8 +99,8 @@ class Passive(CurrentClamp):
         h.stdinit()
         h.run()
 
-        index_v_rest = int(((1000/h.dt)/1000 * self.tstart))
-        index_v_final = int(((1000/h.dt)/1000 * (self.tstart+self.inj_delay)))
+        index_v_rest = int(((1000/h.dt)/1000 * self.inj_delay))
+        index_v_final = int(((1000/h.dt)/1000 * (self.tstop)))
         
         self.v_rest = self.cell_vec[index_v_rest]
         self.v_rest_time = index_v_rest / (1/h.dt)
@@ -114,14 +113,42 @@ class Passive(CurrentClamp):
         self.v_t_const = self.v_rest - (v_diff *.632)
 
         index_v_tau = next(x for x, val in enumerate(list(self.cell_vec)) if val < self.v_t_const) 
-        time_tau = (index_v_tau / ((1000/h.dt)/1000)) - self.tstart
+        time_tau = (index_v_tau / ((1000/h.dt)/1000)) - self.inj_delay
         self.tau = time_tau #/ 1000 (in ms)
         self.r_in = (v_diff)/(0-self.inj_amp) * 1e6 #MegaOhms -> Ohms
         print()
-        print(f"V Rest: {self.v_rest} (mV)")
-        print(f"Resistance: {self.r_in/1e6} (MOhms)")
-        print(f"tau: {self.tau} (ms)")
+        print(f"V Rest: {self.v_rest:.2f} (mV)")
+        print(f"Resistance: {self.r_in/1e6:.2f} (MOhms)")
+        print(f"tau: {self.tau:.2f} (ms)")
         print()
+
+        v_rest_calc = "V_rest Calculation: Voltage taken at time " + str(self.v_rest_time) + "(ms)"
+        v_rest_calc1 = f"{self.v_rest:.2f} (mV)"
+        rin_calc = "R_in Calculation: [(dV/dI)] = (v_start-v_final)/(i_start-i_final)" 
+        rin_calc1 = "(" + str(round(self.v_rest,2)) + "-(" + str(round(self.cell_v_final,2))+"))/(0-(" + str(self.inj_amp) + "))"
+        rin_calc2 = str(round(self.v_rest-self.cell_v_final,2))+ " (mV) /" + str(0-self.inj_amp) + " (nA)"
+        rin_calc3 = str(round(((self.v_rest-self.cell_v_final)/(0-self.inj_amp)),2)) + " (MOhms)"
+
+        tau_calc = "Tau Calculation: [(s) until 63.2% change in mV]"
+        tau_calc1 = "(mV at inj_start_time (" + str(self.inj_delay) + ")) - ((mV at inj_time  - mV at inj_final (" + str(self.tstop) + ")) * 0.632)"
+        tau_calc2 = "(" + str(round(self.v_rest,2)) + ") - (" + str(round(self.v_rest,2)) +"-" +str(round(self.cell_v_final,2))+")*0.632 = " + str(round(self.v_rest - ((self.v_rest - self.cell_v_final)*0.632),2))
+        tau_calc3 = "Time where mV == " + str(round(self.v_t_const,2)) + " = " + str(self.inj_delay+self.tau) + "(ms) | (" + str(round(self.inj_delay+self.tau*1000,2)) + " - v_start_time (" + str(self.inj_delay) +"))/1000"
+        tau_calc4 = str(round(self.tau,4)) + " (ms)"
+
+        print(v_rest_calc)
+        print(v_rest_calc1)
+        print()
+        print(rin_calc)
+        print(rin_calc1)
+        print(rin_calc2)
+        print(rin_calc3)
+        print()
+        print(tau_calc)
+        print(tau_calc1)
+        print(tau_calc2) 
+        print(tau_calc3) 
+        print(tau_calc4)
+        print()  
 
         return (list(self.t_vec), list(self.cell_vec))
         #return (self.v_rest, self.r_in/1e6, self.tau)
@@ -290,19 +317,19 @@ class Profiler():
             plt.figure()
             plt.plot(time_vec, amp_vec)
             plt.title("Passive Cell Current Injection")
-            plt.xlabel('Time (s)')
+            plt.xlabel('Time (ms)')
             plt.ylabel('Membrane Potential (mV)')
             plt.show()
 
         return (time_vec, amp_vec)
     
     def current_injection(self, template_name:str, post_init_function=None, plot=True, 
-                          record_sec="soma", record_loc="0.5", tstart=50, tstop=1000,
-                 inj_sec="soma", inj_loc="0.5", inj_amp=100, inj_delay=100, inj_dur=1000) -> Tuple[list,list]:
+                          record_sec="soma", record_loc="0.5",  tstop=1000,
+                          inj_sec="soma", inj_loc="0.5", inj_amp=100, inj_delay=100, inj_dur=1000) -> Tuple[list,list]:
         
         ccl = CurrentClamp(template_name, post_init_function=post_init_function, 
                                       record_sec=record_sec, record_loc=record_loc,
-                                      tstart=tstart, tstop=tstop, inj_sec=inj_sec, inj_loc=inj_loc,
+                                      tstop=tstop, inj_sec=inj_sec, inj_loc=inj_loc,
                                       inj_amp=inj_amp, inj_delay=inj_delay, inj_dur=inj_dur)
         time_vec, amp_vec = ccl.execute()
 
@@ -310,7 +337,7 @@ class Profiler():
             plt.figure()
             plt.plot(time_vec, amp_vec)
             plt.title("Current Injection")
-            plt.xlabel('Time (s)')
+            plt.xlabel('Time (ms)')
             plt.ylabel('Membrane Potential (mV)')
             plt.show()
         
@@ -346,15 +373,15 @@ class Profiler():
             plt.figure()
             plt.plot(amp_vec, spike_vec)
             plt.title("FI Curve")
-            plt.xlabel('# Spikes')
-            plt.ylabel('Injection (nA)')
+            plt.xlabel('Injection (nA)')
+            plt.ylabel('# Spikes')
             plt.show()
 
         return (amp_vec, spike_vec)
         
 
 #Example usage
-#profiler = Profiler('./temp/templates', './temp/mechanisms/modfiles')
-#profiler.passive_properties('Cell_Cf')
+profiler = Profiler('./temp/templates', './temp/mechanisms/modfiles')
+profiler.passive_properties('Cell_Cf')
 #profiler.fi_curve('Cell_Cf')
 #profiler.current_injection('Cell_Cf', post_init_function="insert_mechs(123)", inj_amp=300, inj_delay=100)
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="bmtool",
-    version="0.3.2",
+    version="0.3.3",
     author="Tyler Banks",
     author_email="tbanks@mail.missouri.edu",
     description="BMTool",
