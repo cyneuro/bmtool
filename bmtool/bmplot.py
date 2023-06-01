rotation fix

bmtool/bmplot.py
@@ -409,12 +409,13 @@ def cell_rotation_3d(**kwargs):
     populations_list = kwargs["populations"]
     config = kwargs["config"]
     group_keys = kwargs["group_by"]
-    title = kwargs["title"]
+    title = kwargs.get("title")
     save_file = kwargs["save_file"]
     quiver_length = kwargs["quiver_length"]
     arrow_length_ratio = kwargs["arrow_length_ratio"]
     group = kwargs["group"]
-    max_cells = kwargs["max_cells"]
+    max_cells = kwargs.get("max_cells",999999999)
+    init_vector = kwargs.get("init_vector","1,0,0")
 
     nodes = util.load_nodes_from_config(config)
 
@@ -480,7 +481,14 @@ def cell_rotation_3d(**kwargs):
             #Convert to arrow direction
             from scipy.spatial.transform import Rotation as R
             uvw = pd.DataFrame([U,V,W]).T
-            rots = R.from_euler('zyx', uvw).as_rotvec().T
+            init_vector = init_vector.split(',')
+            init_vector = np.repeat([init_vector],len(X),axis=0)
+            # To get the final cell orientation after rotation, 
+            # you need to use function Rotaion.apply(init_vec), 
+            # where init_vec is a vector of the initial orientation of a cell
+            rots = R.from_euler('xyz', uvw).apply(init_vector.astype(float))
+            rots = R.from_euler('xyz', pd.DataFrame([rots[:,0],rots[:,1],rots[:,2]]).T).as_rotvec().T
+
             h = ax.quiver(X, Y, Z, rots[0],rots[1],rots[2],color=color,label=group_name, arrow_length_ratio = arrow_length_ratio, length=quiver_length)
             ax.scatter(X,Y,Z,color=color,label=group_name)
             handles.append(h)