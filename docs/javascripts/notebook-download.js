972c6c285f35940ac96acc7173faf8bd08eb62d8
Gregory Glickert <gglick9@gmail.com>
2025-04-03 22:53:12 -0500

    ok button works now

--- docs/javascripts/notebook-download.js
+++ docs/javascripts/notebook-download.js
@@ -4,22 +4,32 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelector('.jp-RenderedHTMLCommon') || 
       document.querySelector('.jp-Cell')) {
     
-    // Get the current URL and replace .html with .ipynb to create download link
+    // Get the current URL path
     const currentPath = window.location.pathname;
-    const notebookPath = currentPath.replace(/\.html$/, '.ipynb');
     
-    // Extract the notebook name from the path
-    const notebookName = notebookPath.split('/').pop();
+    // URL structure is typically like:
+    // /bmtool/site/examples/bmplot/bmplot/
+    // We need to link to:
+    // /bmtool/docs/examples/bmplot/bmplot.ipynb
+    
+    // Extract the relevant part of the path
+    const pathSegments = currentPath.split('/').filter(segment => segment.length > 0);
+    const notebookPath = pathSegments[pathSegments.length - 1] + '.ipynb';
+    const categoryPath = pathSegments[pathSegments.length - 2];
+    
+    // Construct the full path to the original .ipynb file 
+    // We need to use a relative path that goes from the site to the docs
+    const downloadPath = `../../docs/examples/${categoryPath}/${notebookPath}`;
     
     // Create the download button
     const downloadButton = document.createElement('a');
-    downloadButton.href = notebookPath;
+    downloadButton.href = downloadPath;
     downloadButton.className = 'notebook-download-button';
     downloadButton.textContent = 'Download Notebook';
-    downloadButton.download = notebookName;
-    downloadButton.title = 'Download ' + notebookName;
+    downloadButton.download = notebookPath;
+    downloadButton.title = 'Download ' + notebookPath;
     
-    // Get the main content area and insert button at the top
+    // Get the main content area and insert button
     const contentArea = document.querySelector('.md-content__inner');
     if (contentArea) {
       contentArea.appendChild(downloadButton);
