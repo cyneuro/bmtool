b93a24a079b329ad253f5d0cc20f5a72b9b2d2bc
GregGlickert <gglick9@gmail.com>
2026-01-20 13:54:32 -0600

    fix some typos

--- docs/javascripts/collapse-nav.js
+++ docs/javascripts/collapse-nav.js
@@ -1,7 +1,7 @@
 // Collapse navigation sections by default
 document.addEventListener("DOMContentLoaded", function () {
   // Uncheck all nested navigation checkboxes to collapse them
-  document.querySelectorAll(".md-nav__toggle").forEach(function (toggle) {
+  document.querySelectorAll(".md-nav__item--nested .md-nav__toggle").forEach(function (toggle) {
     toggle.checked = false;
   });
 });
